ae3c264002a80cf2f22f889f3d8a5e245023377d
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-30 09:55:13 +0700

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,21 +1,18 @@
 import styles, { layout } from "../styles";
 
 const Testimonials = () => (
-  <section
-    id="clients"
-    className={`${styles.paddingY} ${layout.section}`}
-  >
-    {/* Gambar Kiri */}
-    <div className={layout.sectionImg}>
+  <section id="clients" className={`${styles.paddingY} flex flex-col-reverse sm:${layout.section}`}>
+    {/* Gambar */}
+    <div className={`${layout.sectionImg} sm:order-none order-2`}>
       <img
         src="/photos/image-lalulintas.png"
         alt="Pelanggaran Lalu Lintas"
-        className="w-[100%] h-[100%] object-cover rounded-[20px]"
+        className="w-full h-full object-cover rounded-[20px]"
       />
     </div>
 
-    {/* Teks Kanan */}
-    <div className={`${layout.sectionInfo} pl-10`}>
+    {/* Teks */}
+    <div className={`${layout.sectionInfo} pl-0 sm:pl-10 sm:order-none order-1`}>
       <h2 className={styles.heading2}>
         Pelanggaran <br className="sm:block hidden" /> Lalu Lintas
       </h2>
