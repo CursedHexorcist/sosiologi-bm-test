b102c9742f46adbe141dc8a800991144df800e86
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-30 08:34:39 +0700

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,45 +1,30 @@
-import { apple, bill, google } from "../assets";
 import styles, { layout } from "../styles";
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
-    {/* Left Side */}
+    {/* Left Side: Image */}
     <div className={layout.sectionImgReverse}>
-      {/* Background Image */}
+      {/* Gambar Tawuran */}
       <img
-        src={bill}
-        alt="Billing"
-        className="w-[100%] h-[100%] relative z-[5]"
+        src="/photos/image-tawuran.webp"
+        alt="Tawuran Remaja"
+        className="w-[100%] h-[100%] relative z-[5] object-cover rounded-[20px]"
       />
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
     </div>
 
-    {/* Right Side */}
+    {/* Right Side: Text Info */}
     <div className={layout.sectionInfo}>
-      {/* Info */}
       <h2 className={styles.heading2}>
-        Easily control your
-        <br className="sm:block hidden" /> billing & invoicing.
+        Tawuran <br className="sm:block hidden" /> Remaja
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
-        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+        Tawuran antarpelajar atau kelompok remaja merupakan bentuk kekerasan sosial yang
+        menyimpang dari norma hukum dan norma kesopanan. Hal ini sering dipicu oleh hal
+        sepele, seperti perbedaan sekolah atau geng, dan dapat merugikan banyak pihak,
+        termasuk masyarakat sekitar.
       </p>
-
-      {/* Download */}
-      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img
-          src={apple}
-          alt="App Store"
-          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
-        />
-        <img
-          src={google}
-          alt="Google Play Store"
-          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
-        />
-      </div>
     </div>
   </section>
 );
